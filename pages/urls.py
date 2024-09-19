78b21d7098c89f1471b997dba0aea1eeab419960
Anudeep <agudimalla99@gmail.com>
2024-09-19 12:44:25 -0400

    fouth commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include   #new
+from . import views
 
-from .views import home_page_view
+# from .views import home_page_view
 
 urlpatterns = [
     path("admin/", admin.site.urls),
