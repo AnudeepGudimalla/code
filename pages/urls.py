b0fca07bcc00122ba46e7e14f64b4e897a41bc23
Anudeep <agudimalla99@gmail.com>
2024-09-19 12:29:51 -0400

    second commit

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include   #new
 
 from .views import home_page_view
 
 urlpatterns = [
-    path("", home_page_view),
+    path("admin/", admin.site.urls),
+    path("", include("pages.urls")),     #new
 ]
